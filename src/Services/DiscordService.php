7c4d43c33f3476d2eb4370119255f1336d5c3292
Guido Gasper <guido.gasper@googlemail.com>
2021-12-17 14:20:41 +0100

    - Simplified the service class by removing unneeded methods. - Service class is now stateless.

--- src/Services/DiscordService.php
+++ src/Services/DiscordService.php
@@ -8,48 +8,20 @@ use Khazl\Discord\Contracts\DiscordServiceInterface;
 class DiscordService implements DiscordServiceInterface
 {
 
-    private $payload;
-
-    function __construct()
-    {
-
-    }
-
     /**
      * @param string $hookName
      * @param string $message
      */
     public function send(string $hookName, string $message): void
     {
-        $this->setName();
-        $this->setMessage($message);
+        $payload = [
+            'username' => config('discord.name', 'Captain Hook'),
+            'content' => $message
+        ];
 
-        Http::post($this->getHook($hookName), $this->payload);
-    }
-
-    private function setName(): void
-    {
-        $username = config('discord.name');
-        if ($username) {
-            $this->payload['username'] = $username;
-        }
-    }
-
-    /**
-     * @param string $message
-     */
-    private function setMessage(string $message): void
-    {
-        $this->payload['content'] = $message;
-    }
-
-    /**
-     * @param string $hookName
-     * @return string
-     */
-    private function getHook(string $hookName): string
-    {
-        return config('discord.base_url').
+        $hook = config('discord.base_url').
             config("discord.hooks.{$hookName}", 'undefined-hook');
+
+        Http::post($hook, $payload);
     }
 }
